0ce984c5fc3bc5bee59b6db849a55fbaf0eb6107
Roman Chyla <roman.chyla@gmail.com>
2018-08-20 17:27:15 -0400

    better

---
+++ src/jfun/src/components/ListOfSelectedArticles.vue
@@ -0,0 +1,36 @@
+
+
+<script>
+import ListOfArticles from './ListOfArticles'
+import * as _ from 'lodash'
+
+export default {
+    extends: ListOfArticles,
+    methods: {
+        getRef: function() {
+            debugger
+            return 'selectedArticles'
+        },
+        getSortKey: function() {
+            return 'relevant'
+        },
+        getItems: function() {
+            debugger;
+            let out = _.clone(this.items)
+            
+            _.remove(out, function(value, index, array) {
+            if (value.relevant < 0)
+                return true
+            })
+            
+            // re-sort by relevancy
+            out = _.orderBy(out, ['relevant'])
+            return out;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

5b8176200ed920c2ac2c50e962a3c7da4bc39433
Roman Chyla <roman.chyla@gmail.com>
2018-09-05 23:22:59 -0400

    DB modifications

---
+++ src/alembic/versions/3df2e100fb79_modify_progress_column_type.py
@@ -0,0 +1,27 @@
+"""Modify progress column type
+
+Revision ID: 3df2e100fb79
+Revises: 3b47cb6a817
+Create Date: 2018-09-05 23:13:58.552703
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3df2e100fb79'
+down_revision = '3b47cb6a817'
+
+from alembic import op
+import sqlalchemy as sa
+
+                               
+
+
+def upgrade():
+    with op.batch_alter_table('experiments') as batch_op:
+        batch_op.drop_column('progress')
+        batch_op.add_column(sa.Column('experiment_results', sa.FLOAT))
+
+def downgrade():
+    with op.batch_alter_table('experiments') as batch_op:
+        batch_op.drop_column('progress')
+        batch_op.add_column(sa.Column('experiment_results', sa.Integer))
